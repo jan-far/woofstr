636bd5e8fb0c2512a23a4d519faa30cf89c5e415
David Rhodes <david@espressocode.tech>
2021-09-06 21:20:27 -0500

    test

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,6 +1,7 @@
-import { CircularProgress } from '@material-ui/core';
-import { PauseRounded, PlayArrowRounded } from '@material-ui/icons';
-import React, { useState, useRef, useEffect } from 'react';
+// import { CircularProgress } from '@material-ui/core';
+// import { PauseRounded, PlayArrowRounded } from '@material-ui/icons';
+// import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
 import './AudioPlayer.css';
 import ReactAudioPlayer from 'react-audio-player';
 
@@ -12,126 +13,126 @@ export default function AudioPlayer({
   roomID,
   id,
 }) {
-  const [isPlaying, setPlaying] = useState(false);
-  const [isMediaLoaded, setIsMediaLoaded] = useState(false);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [isMetaDataLoaded, setIsMetaDataLoaded] = useState(false);
-  const [sliderValue, setSliderValue] = useState(0);
-  const [duration, setDuration] = useState(false);
+  // const [isPlaying, setPlaying] = useState(false);
+  // const [isMediaLoaded, setIsMediaLoaded] = useState(false);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [isMetaDataLoaded, setIsMetaDataLoaded] = useState(false);
+  // const [sliderValue, setSliderValue] = useState(0);
+  // const [duration, setDuration] = useState(false);
 
-  const totalDuration = useRef('');
-  const audio = useRef(new Audio(audioUrl));
-  const interval = useRef();
-  const isUploading = useRef(audioUrl === 'uploading');
+  // const totalDuration = useRef('');
+  // const audio = useRef(new Audio(audioUrl));
+  // const interval = useRef();
+  // const isUploading = useRef(audioUrl === 'uploading');
 
-  React.useEffect(() => {
-    if (isUploading.current && audioUrl !== 'uploading') {
-      audio.current = new Audio(audioUrl);
-      audio.current.load();
-      setIsLoaded(true);
-    } else if (isUploading.current === false) {
-      setIsLoaded(true);
-    }
-  }, [audioUrl]);
+  // React.useEffect(() => {
+  //   if (isUploading.current && audioUrl !== 'uploading') {
+  //     audio.current = new Audio(audioUrl);
+  //     audio.current.load();
+  //     setIsLoaded(true);
+  //   } else if (isUploading.current === false) {
+  //     setIsLoaded(true);
+  //   }
+  // }, [audioUrl]);
 
-  function getAudioDuration(media) {
-    return new Promise((resolve) => {
-      media.onloadedmetadata = () => {
-        media.currentTime = Number.MAX_SAFE_INTEGER;
-        media.ontimeupdate = () => {
-          media.ontimeupdate = () => {};
-          media.currentTime = 0.1;
-          resolve(media.duration);
-        };
-      };
-    });
-  }
+  // function getAudioDuration(media) {
+  //   return new Promise((resolve) => {
+  //     media.onloadedmetadata = () => {
+  //       media.currentTime = Number.MAX_SAFE_INTEGER;
+  //       media.ontimeupdate = () => {
+  //         media.ontimeupdate = () => {};
+  //         media.currentTime = 0.1;
+  //         resolve(media.duration);
+  //       };
+  //     };
+  //   });
+  // }
 
-  useEffect(() => {
-    if (isLoaded) {
-      getAudioDuration(audio.current).then(() => {
-        setIsMetaDataLoaded(true);
-      });
-    }
-  }, [isLoaded]);
+  // useEffect(() => {
+  //   if (isLoaded) {
+  //     getAudioDuration(audio.current).then(() => {
+  //       setIsMetaDataLoaded(true);
+  //     });
+  //   }
+  // }, [isLoaded]);
 
-  useEffect(() => {
-    if (isMetaDataLoaded) {
-      audio.current.addEventListener('canplaythrough', () => {
-        if (!totalDuration.current) {
-          setIsMediaLoaded(true);
-          const time = formatTime(audio.current.duration);
-          totalDuration.current = time;
-          setDuration(totalDuration.current);
-        }
-      });
+  // useEffect(() => {
+  //   if (isMetaDataLoaded) {
+  //     audio.current.addEventListener('canplaythrough', () => {
+  //       if (!totalDuration.current) {
+  //         setIsMediaLoaded(true);
+  //         const time = formatTime(audio.current.duration);
+  //         totalDuration.current = time;
+  //         setDuration(totalDuration.current);
+  //       }
+  //     });
 
-      audio.current.addEventListener('ended', () => {
-        clearInterval(interval.current);
-        setDuration(totalDuration.current);
-        setSliderValue(0);
-        setPlaying(false);
-      });
-    }
-  }, [isMetaDataLoaded]);
+  //     audio.current.addEventListener('ended', () => {
+  //       clearInterval(interval.current);
+  //       setDuration(totalDuration.current);
+  //       setSliderValue(0);
+  //       setPlaying(false);
+  //     });
+  //   }
+  // }, [isMetaDataLoaded]);
 
-  useEffect(() => {
-    if (audioID !== id) {
-      audio.current.pause();
-      setPlaying(false);
-    }
-  }, [audioID, id]);
+  // useEffect(() => {
+  //   if (audioID !== id) {
+  //     audio.current.pause();
+  //     setPlaying(false);
+  //   }
+  // }, [audioID, id]);
 
-  function formatTime(time) {
-    let minutes = Math.floor(time / 60);
-    let seconds = Math.floor(time - minutes * 60);
+  // function formatTime(time) {
+  //   let minutes = Math.floor(time / 60);
+  //   let seconds = Math.floor(time - minutes * 60);
 
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
-    }
-    return `${minutes}:${seconds}`;
-  }
+  //   if (minutes < 10) {
+  //     minutes = `0${minutes}`;
+  //   }
+  //   if (seconds < 10) {
+  //     seconds = `0${seconds}`;
+  //   }
+  //   return `${minutes}:${seconds}`;
+  // }
 
-  function playAudio() {
-    setPlaying(true);
-    audio.current.play();
-    if (audioID !== id) {
-      setAudioID(id);
-    }
-    interval.current = setInterval(updateSlider, 100);
-  }
+  // function playAudio() {
+  //   setPlaying(true);
+  //   audio.current.play();
+  //   if (audioID !== id) {
+  //     setAudioID(id);
+  //   }
+  //   interval.current = setInterval(updateSlider, 100);
+  // }
 
-  function updateSlider() {
-    let sliderPosition = 0;
-    const { currentTime, duration } = audio.current;
-    if (typeof duration === 'number') {
-      sliderPosition = currentTime * (100 / duration);
-      setSliderValue(sliderPosition);
-      const time = formatTime(currentTime);
-      setDuration(time);
-    }
-  }
+  // function updateSlider() {
+  //   let sliderPosition = 0;
+  //   const { currentTime, duration } = audio.current;
+  //   if (typeof duration === 'number') {
+  //     sliderPosition = currentTime * (100 / duration);
+  //     setSliderValue(sliderPosition);
+  //     const time = formatTime(currentTime);
+  //     setDuration(time);
+  //   }
+  // }
 
-  function stopAudio() {
-    audio.current.pause();
-    clearInterval(interval.current);
-    setPlaying(false);
-    setDuration(totalDuration.current);
-  }
+  // function stopAudio() {
+  //   audio.current.pause();
+  //   clearInterval(interval.current);
+  //   setPlaying(false);
+  //   setDuration(totalDuration.current);
+  // }
 
-  function scrubAudio(event) {
-    const value = event.target.value;
-    const { duration } = audio.current;
+  // function scrubAudio(event) {
+  //   const value = event.target.value;
+  //   const { duration } = audio.current;
 
-    if (isMediaLoaded) {
-      const seekTo = duration * (value / 100);
-      audio.current.currentTime = seekTo;
-      setSliderValue(value);
-    }
-  }
+  //   if (isMediaLoaded) {
+  //     const seekTo = duration * (value / 100);
+  //     audio.current.currentTime = seekTo;
+  //     setSliderValue(value);
+  //   }
+  // }
 
   return (
     <>
@@ -159,7 +160,7 @@ export default function AudioPlayer({
           />
         </div> */}
       </div>
-      <span className="chat__timestamp audioplayer__time">{duration}</span>
+      {/* <span className="chat__timestamp audioplayer__time">{duration}</span> */}
     </>
   );
 }
